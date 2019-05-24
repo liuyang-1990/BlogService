6f8fac4adfde41d53b20ba66749669d93f401bd1
Luis.Y.Liu <luis.y.liu@newegg.com>
2019-05-24 17:26:09 +0800

    add unit test

---
+++ test/Blog.Test/Infrastructure/JwtHelperUnitTest.cs
@@ -0,0 +1,94 @@
+﻿using Blog.Infrastructure;
+using Blog.Infrastructure.Implement;
+using Blog.Model;
+using Blog.Model.Settings;
+using Microsoft.Extensions.Options;
+using Moq;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Blog.Test.Infrastructure
+{
+    public class JwtHelperUnitTest
+    {
+        private readonly Mock<IOptions<JwtConfig>> _jwtConfig;
+        private readonly Mock<IRedisHelper> _redisHelper;
+        public JwtHelperUnitTest()
+        {
+            _jwtConfig = new Mock<IOptions<JwtConfig>>();
+            _redisHelper = new Mock<IRedisHelper>();
+            _jwtConfig.Setup(x => x.Value).Returns(() => new JwtConfig()
+            {
+                Audience = "test",
+                Issuer = "test",
+                SecurityKey = Guid.NewGuid().ToString()
+            });
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void IssueJwt_Test(bool isRefresh)
+        {
+            var helper = new JwtHelper(_jwtConfig.Object, _redisHelper.Object);
+            var res = helper.IssueJwt(new JwtToken()
+            {
+                Uid = 1,
+                Role = "Admin"
+            }, isRefresh);
+            Assert.NotNull(res);
+        }
+
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void RefreshJwt_Test_Null(bool contains, string value)
+        {
+            _redisHelper.Setup(x => x.ContainsKey("refresh_token_1")).Returns(contains);
+            _redisHelper.Setup(x => x.Get<string>("refresh_token_1")).Returns(value);
+            var helper = new JwtHelper(_jwtConfig.Object, _redisHelper.Object);
+            var res = helper.RefreshJwt("test", new JwtToken()
+            {
+                Uid = 1,
+                Role = "Admin"
+            });
+            Assert.Null(res);
+
+        }
+
+        public static List<object[]> Data = new List<object[]>()
+        {
+            new object[]
+            {
+               false,
+               null
+            },
+            new object[]
+            {
+                true,
+                null
+            },
+            new object[]
+            {
+                true,
+                "test1"
+            }
+        };
+
+
+        [Fact]
+        public void RefreshJwt_Test_Ok()
+        {
+            _redisHelper.Setup(x => x.ContainsKey("refresh_token_1")).Returns(true);
+            _redisHelper.Setup(x => x.Get<string>("refresh_token_1")).Returns("test");
+            var helper = new JwtHelper(_jwtConfig.Object, _redisHelper.Object);
+            var res = helper.RefreshJwt("test", new JwtToken()
+            {
+                Uid = 1,
+                Role = "Admin"
+            });
+            Assert.NotNull(res);
+
+        }
+    }
+}
---
+++ test/Blog.Test/Infrastructure/Md5HelperUnitTest.cs
@@ -0,0 +1,18 @@
+﻿using Blog.Infrastructure.Implement;
+using Xunit;
+
+namespace Blog.Test.Infrastructure
+{
+    public class Md5HelperUnitTest
+    {
+        [Theory]
+        [InlineData(null, null)]
+        [InlineData("123", "202cb962ac59075b964b07152d234b70")]
+        public void Encrypt32_Test(string originStr, string result)
+        {
+            var helper = new Md5Helper();
+            var actual = helper.Encrypt32(originStr);
+            Assert.Equal(result, actual);
+        }
+    }
+}
