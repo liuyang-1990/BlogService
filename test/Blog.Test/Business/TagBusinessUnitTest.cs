5f9c548f637efaa301b5623357894c35ef9147f1
Luis.Y.Liu <luis.y.liu@newegg.com>
2019-05-25 14:32:03 +0800

    add tagBusiness unit test

---
+++ test/Blog.Test/Business/TagBusinessUnitTest.cs
@@ -0,0 +1,103 @@
+﻿using Blog.Business.Implement;
+using Blog.Model;
+using Blog.Model.Db;
+using Blog.Model.Request;
+using Blog.Model.Response;
+using Blog.Model.ViewModel;
+using Blog.Repository;
+using Moq;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Blog.Test.Business
+{
+    public class TagBusinessUnitTest
+    {
+        private readonly Mock<ITagRepository> _tagRespoitory;
+        private readonly TagBusiness _tagBusiness;
+
+        public TagBusinessUnitTest()
+        {
+            _tagRespoitory = new Mock<ITagRepository>();
+            _tagBusiness = new TagBusiness(_tagRespoitory.Object);
+        }
+
+
+        [Theory]
+        [MemberData(nameof(Data))]
+        public async Task Insert_Test(TagInfo tagInfo, bool isExist, ResultModel<string> expectedModel)
+        {
+            _tagRespoitory.Setup(x => x.IsExist(tagInfo, UserAction.Add)).ReturnsAsync(() => isExist);
+            _tagRespoitory.Setup(x => x.Insert(tagInfo)).ReturnsAsync(() => true);
+            var actualModel = await _tagBusiness.Insert(tagInfo);
+            var actualStr = JsonConvert.SerializeObject(actualModel);
+            var expectedStr = JsonConvert.SerializeObject(expectedModel);
+            Assert.Equal(expectedStr, actualStr);
+        }
+
+        public static List<object[]> Data = new List<object[]>()
+        {
+            new object[]
+            {
+                new TagInfo(),
+                true,
+                new ResultModel<string>()
+                {
+                    Status="2"
+                }
+            },
+            new object[]
+            {
+                new TagInfo(),
+                false,
+                new ResultModel<string>()
+                {
+                    IsSuccess = true,
+                    Status="0"
+                }
+            }
+        };
+
+        [Fact]
+        public async Task GetPageList_Test()
+        {
+            var expectedModel = new JsonResultModel<TagInfo>()
+            {
+                Rows = new List<TagInfo>(),
+                TotalRows = 10
+            };
+            _tagRespoitory
+                .Setup(x => x.GetPageList(It.IsAny<GridParams>(), It.IsAny<Expression<Func<TagInfo, bool>>>()))
+                .ReturnsAsync(() => expectedModel);
+            var actualModel = await _tagBusiness.GetPageList(new GridParams(), "");
+            var actualStr = JsonConvert.SerializeObject(actualModel);
+            var expectedStr = JsonConvert.SerializeObject(expectedModel);
+            Assert.Equal(expectedStr, actualStr);
+        }
+
+        [Fact]
+        public async Task GetAllTags_Test()
+        {
+            _tagRespoitory.Setup(x => x.GetAllTags()).ReturnsAsync(() => new List<TagInfo>() { new TagInfo() });
+            var actualModel = await _tagBusiness.GetAllTags();
+            Assert.Single(actualModel);
+        }
+
+        [Theory]
+        [MemberData(nameof(Data))]
+        public async Task Update_Test(TagInfo tagInfo, bool isExist, ResultModel<string> expectedModel)
+        {
+            _tagRespoitory.Setup(x => x.IsExist(tagInfo, UserAction.Update)).ReturnsAsync(() => isExist);
+            _tagRespoitory.Setup(x => x.Update(tagInfo)).ReturnsAsync(() => true);
+            var actualModel = await _tagBusiness.Update(tagInfo);
+            var actualStr = JsonConvert.SerializeObject(actualModel);
+            var expectedStr = JsonConvert.SerializeObject(expectedModel);
+            Assert.Equal(expectedStr, actualStr);
+        }
+
+    }
+}
