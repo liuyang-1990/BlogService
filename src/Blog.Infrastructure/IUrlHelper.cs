6b431387ccd4f9b5722a20ee4847948c9f2af586
liuyang-1990 <xy_liuy0305@163.com>
2019-07-20 20:50:53 +0800

    add urlhelper

---
+++ src/Blog.Infrastructure/IUrlHelper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.Infrastructure
+{
+    public interface IUrlHelper
+    {
+        string GetQueryString(string url, string para);
+    }
+}
