3c70af35c545fc7b20e96d390fe631a5119f1280
liuyang-1990 <xy_liuy0305@163.com>
2019-07-23 22:16:33 +0800

    fix dbcontext

--- src/Blog.Repository/Dao/DbContext.cs
+++ src/Blog.Repository/Dao/DbContext.cs
@@ -1,24 +1,20 @@
-﻿using Blog.Infrastructure;
+﻿using Autofac;
+using Blog.Infrastructure;
+using Microsoft.Extensions.Configuration;
 using SqlSugar;
 using StackExchange.Profiling;
 using System.Linq;
-using Autofac;
-using Microsoft.Extensions.Configuration;
 
 namespace Blog.Repository.Dao
 {
     public class DbContext
     {
-        /// <summary>
-        /// 用来处理事务多表查询和复杂的操作
-        /// </summary>
-        private SqlSugarClient _db;
 
-
-        private DbContext()
+        public static SqlSugarClient GetDbContext()
         {
             var configuration = CoreContainer.Instance.Resolve<IConfiguration>();
-            _db = new SqlSugarClient(new ConnectionConfig()
+            //用来处理事务多表查询和复杂的操作
+            var db = new SqlSugarClient(new ConnectionConfig()
             {
                 ConnectionString = configuration["ConnectionStrings:ConnectionString"],
                 DbType = DbType.MySql,
@@ -26,7 +22,7 @@ namespace Blog.Repository.Dao
                 IsAutoCloseConnection = true
             });
 
-            _db.QueryFilter.Add(new SqlFilterItem()
+            db.QueryFilter.Add(new SqlFilterItem()
             {
                 //单表全局过滤器
                 FilterValue = filterdb => new SqlFilterResult() { Sql = "   is_deleted=0" },
@@ -38,19 +34,13 @@ namespace Blog.Repository.Dao
                 IsJoinQuery = true
             });
             //调式代码 用来打印SQL 
-            _db.Aop.OnLogExecuting = (sql, pars) =>
+            db.Aop.OnLogExecuting = (sql, pars) =>
             {
                 var sqlP = sql + "\r\n" +
-                           _db.Utilities.SerializeObject(pars.ToDictionary(it => it.ParameterName, it => it.Value));
+                           db.Utilities.SerializeObject(pars.ToDictionary(it => it.ParameterName, it => it.Value));
                 MiniProfiler.Current.CustomTiming("[SQL]:", sqlP);
             };
-        }
-
-        public static SqlSugarClient GetDbContext()
-        {
-
-            var dbContext = new DbContext();
-            return dbContext._db;
+            return db;
 
         }
     }
