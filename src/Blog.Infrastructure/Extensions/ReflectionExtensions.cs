10db957eb5d8216750f34d01b659996c30214ff8
ll5u <luis.y.liu@newegg.com>
2019-10-25 18:04:22 +0800

    fix ReflectionExtensions

--- src/Blog.Infrastructure/Extensions/ReflectionExtensions.cs
+++ src/Blog.Infrastructure/Extensions/ReflectionExtensions.cs
@@ -15,16 +15,33 @@ namespace Blog.Infrastructure.Extensions
         {
             return PropertyValueGetters.GetOrAdd(propertyInfo, prop =>
             {
-                var parameterExpression = Expression.Parameter(typeof(object), "obj");
-                return (Func<object, object>)Expression.Lambda(Expression.Convert(Expression.Call(propertyInfo.DeclaringType.IsValueType ? Expression.Unbox(parameterExpression, propertyInfo.DeclaringType) : Expression.Convert(parameterExpression, propertyInfo.DeclaringType), prop.GetGetMethod()), typeof(object)), parameterExpression).Compile();
+                var instance = Expression.Parameter(typeof(object), "obj");
+                var getterCall = Expression.Call(propertyInfo.DeclaringType.IsValueType ?
+                      Expression.Unbox(instance, propertyInfo.DeclaringType) :
+                      Expression.Convert(instance, propertyInfo.DeclaringType), prop.GetGetMethod());
+                var castToObject = Expression.Convert(getterCall, typeof(object));
+                return Expression.Lambda(castToObject, instance).Compile() as Func<object, object>;
             });
         }
 
         public static Action<object, object> GetValueSetter(this PropertyInfo propertyInfo)
         {
-            var parameterExpression1 = Expression.Parameter(typeof(object), "obj");
-            var parameterExpression2 = Expression.Parameter(typeof(object), "obj");
-            return PropertyValueSetters.GetOrAdd(propertyInfo, prop => Expression.Lambda<Action<object, object>>(Expression.Call(propertyInfo.DeclaringType.IsValueType ? Expression.Unbox(parameterExpression1, propertyInfo.DeclaringType) : Expression.Convert(parameterExpression1, propertyInfo.DeclaringType), propertyInfo.GetSetMethod(), (Expression)Expression.Convert(parameterExpression2, propertyInfo.PropertyType)), parameterExpression1, parameterExpression2).Compile());
+            return PropertyValueSetters.GetOrAdd(propertyInfo, prop =>
+            {
+                var obj = Expression.Parameter(typeof(object), "o");
+                var value = Expression.Parameter(typeof(object));
+                // Note that we are using Expression.Unbox for value types and Expression.Convert for reference types
+                var expr =
+                    Expression.Lambda<Action<object, object>>(
+                        Expression.Call(
+                            propertyInfo.DeclaringType.IsValueType
+                                ? Expression.Unbox(obj, propertyInfo.DeclaringType)
+                                : Expression.Convert(obj, propertyInfo.DeclaringType),
+                            propertyInfo.GetSetMethod(),
+                            Expression.Convert(value, propertyInfo.PropertyType)),
+                        obj, value);
+                return expr.Compile();
+            });
         }
     }
 }
