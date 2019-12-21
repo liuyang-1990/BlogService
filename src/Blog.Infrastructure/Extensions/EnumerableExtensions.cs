e7b85e810cbaa01f2f9cc4f64c340a7c94892bc4
ll5u <luis.y.liu@newegg.com>
2019-12-21 15:45:50 +0800

    add extensions

---
+++ src/Blog.Infrastructure/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Blog.Infrastructure.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static bool IsNullOrEmpty<TSource>(this IEnumerable<TSource> source)
+        {
+            return source == null || !source.Any();
+        }
+
+        public static void ForEach<TSource>(this IEnumerable<TSource> source, Action<TSource> action)
+        {
+            if (action == null)
+            {
+                throw new ArgumentNullException(nameof(action));
+            }
+            if (source.IsNullOrEmpty()) return;
+            foreach (var item in source)
+            {
+                action(item);
+            }
+        }
+    }
+}
---
+++ src/Blog.Infrastructure/Extensions/StringExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Blog.Infrastructure.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string RemovePostFix(this string str, params string[] postFixes)
+        {
+            if (str == null)
+            {
+                return null;
+            }
+            if (str == string.Empty)
+            {
+                return string.Empty;
+            }
+            if (postFixes.IsNullOrEmpty())
+            {
+                return str;
+            }
+
+            foreach (var postFix in postFixes)
+            {
+                if (str.EndsWith(postFix))
+                {
+                    return str.Left(str.Length - postFix.Length);
+                }
+            }
+            return str;
+        }
+
+        public static string Left(this string str, int len)
+        {
+            if (str == null)
+            {
+                throw new ArgumentNullException(nameof(str));
+            }
+
+            if (str.Length < len)
+            {
+                throw new ArgumentException("len argument can not be bigger than given string's length!");
+            }
+
+            return str.Substring(0, len);
+        }
+    }
+}
